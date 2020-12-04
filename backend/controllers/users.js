bec29b730f9125214a155f3ea17c459912ef3003
Tatu Nordström <Tatu.Nordstrom@metropolia.fi>
2020-12-04 15:48:55 +0200

    email & phone for user

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -19,6 +19,8 @@ usersRouter.post('/', async (request, response) => {
     const user = new User ({
         username: body.username,
         name: body.name,
+        email: body.email,
+        phone: body.phone,
         passwordHash
     })
 
